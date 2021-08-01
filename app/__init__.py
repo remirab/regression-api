d2a1ea579d73fcc6f351c359a75bac15e1343dab
Reza Mirabizadeh <reza.mirabizadeh@nanos.ai>
2021-08-01 13:58:15 +0430

    feat: App folders structure check

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,17 @@ from app.utility import YamlParser, Helpers
 from app.app_factory import AppFactory
 
 __all__ = ["Dataset", "Model", "YamlParser", "Helpers", "AppFactory"]
+
+
+import os
+from constants import Constants
+constants = Constants()
+
+if not os.path.exists(constants.JUPYTER_DIR):
+    os.mkdir(constants.JUPYTER_DIR)
+
+if not os.path.exists(constants.DATASETS_DIR):
+    os.mkdir(constants.DATASETS_DIR)
+
+if not os.path.exists(constants.IMAGES_DIR):
+    os.mkdir(constants.IMAGES_DIR)
